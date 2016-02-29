91f314ad11a0f5d6dea9bb27fa7264819984b0ec
Howard Wang <howardwang00@gmail.com>
2016-02-28 16:00:45 -0800

    Combined .c libraries with their .h counterparts.

--- 2016 Regionals/Create/main.c
+++ 2016 Regionals/Create/main.c
@@ -1,7 +1,20 @@
 // Created on Sun January 31 2016
 
+#include "createDrive.c";
+#include <createDrive.h>;
+#include <generic.c>;
+#include <generic.h>;
+#include <newmenu.c>;
+#include <newmenu.h>;
+
 int main()
 {
-	printf("Hello, World!\n");
+	
+	create_connect();
+	create_drive_direct_dist(50, 50, 100);
+	create_block();
+	
+	
+	create_disconnect();
 	return 0;
 }
--- 2016 Regionals/Link/main.c
+++ 2016 Regionals/Link/main.c
@@ -1,7 +1,18 @@
 // Created on Sun January 31 2016
 
+#include <drive.c>;
+#include <drive.h>;
+#include <generic.c>;
+#include <generic.h>;
+#include <newmenu.c>;
+#include <newmenu.h>;
+
 int main()
 {
-	printf("Hello, World!\n");
+	enable_servos();
+	forward(100);
+	
+	
+	disable_servos();
 	return 0;
 }
