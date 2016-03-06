74c1b394db1fc2e430e64f07591c73262ce12227
whackazombie <kevingao26@gmail.com>
2016-03-06 15:06:58 -0800

    testing create

--- 2016 Regionals/Create/main.c
+++ 2016 Regionals/Create/main.c
@@ -1,17 +1,17 @@
 // Created on Sun January 31 2016
 
-#include "createDrive.c";
 #include <createDrive.h>;
-#include <generic.c>;
 #include <generic.h>;
-#include <newmenu.c>;
 #include <newmenu.h>;
+#include <singing.h>;
 
 int main()
 {
 	
 	create_connect();
-	create_drive_direct_dist(50, 50, 100);
+	create_forward(50, 100);
+	create_right(90, 30, 100);
+	create_forward(50, 100);
 	create_block();
 	
 	
