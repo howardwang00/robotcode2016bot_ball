03b48f05b927816d5de5804068ce37708bbc1066
Howard Wang <howardwang00@gmail.com>
2016-03-13 16:26:32 -0700

    Worked on Link driving and claw movement.

--- 2016 Regionals/Link/functions.h
+++ 2016 Regionals/Link/functions.h
@@ -7,34 +7,69 @@
 #include "drive.h"
 #include "generic.h"
 
-#define MOT_LEFT 2	//left wheel port
-#define MOT_RIGHT 3	//right wheel port
+//two motors are in drive.h
+//#define MOT_LEFT 1	//left wheel port
+//#define MOT_RIGHT 0	//right wheel port
 #define MAIN_ARM 3	//main arm port
 #define CLAW 2	//claw port
 
 #define ARM_DOWN 380	//arm down position, the arm is down on the ground
 #define ARM_UP 1871		//arm up position, for dumping in box
 #define ARM_DRIVE 720	//arm position for driving
-#define CLAW_OPEN 100	//claw open position
-#define CLAW_CLOSE 100	//claw close position
+#define CLAW_OPEN 0	//claw open position
+#define CLAW_CLOSE 400	//claw close position
+
+//camera code
+#define RED 0	//for camera
+#define GREEN 1
+#define NOTHING 234	//random #
 
 void servo(int port, int position) {
 	set_servo_position(port, position);
 }
 void arm_down() {
 	servo(MAIN_ARM, ARM_DOWN);
+	msleep(50);	//wait to prevent servo shaking
 }
 void arm_up() {
 	servo(MAIN_ARM, ARM_UP);
+	msleep(50);
 }
 void arm_drive() {
 	servo(MAIN_ARM, ARM_DRIVE);
+	msleep(50);
 }
 void claw_open() {
 	servo(CLAW, CLAW_OPEN);
+	msleep(50);
 }
 void claw_close() {
 	servo(CLAW, CLAW_CLOSE);
+	msleep(50);
+}
+void collect_poms() {
+	arm_down();
+	msleep(500);
+	claw_close();
+	msleep(500);
+	arm_up();
+	msleep(500);
+	claw_open();
+	msleep(500);
+}
+int check_poms() {
+	camera_open();
+	camera_update();	//update many times for optimal results
+	camera_update();
+	camera_update();
+	camera_update();
+	camera_update();
+	int green_pom_area = get_object_area(GREEN, 0);
+	int red_pom_area = get_object_area(RED, 0);
+	printf("Green: %d", green_pom_area);
+	printf("Red: %d", red_pom_area);
+	
+	return 0;	//no color blobs of either type detected
 }
 
 
--- 2016 Regionals/Link/main.c
+++ 2016 Regionals/Link/main.c
@@ -9,14 +9,57 @@
 
 //#defines in functions.h
 
-int main()
-{
+//#define MAIN
+#ifdef main
+int main() {
+	
+	
+	shut_down_in(119.5);
 	enable_servos();
-	forward(20);
-	msleep(1000);
+	
+	
+	
+	disable_servos();
+}
+
+#endif
+
+#define DRIVEPATH_TEST
+#ifdef DRIVEPATH_TEST
+int main() {
+	printf("In Drivepath test");
+	
+	//light_start(0);
+	shut_down_in(119.5);
+	arm_drive();
+	claw_open();
+	enable_servos();
+	
+	forward(100);
+	/*
+	msleep(500);
+	collect_poms();
+	*/
+	
+	
+}
+
+#endif
+
+//#define ARM_CLAW_TEST
+#ifdef ARM_CLAW_TEST
+int main() {
+	enable_servos();
+	
 	arm_up();
 	msleep(1000);
+	claw_open();
+	msleep(1000);
 	arm_down();
+	msleep(1000);
+	claw_close();
+	msleep(5000);
+	arm_up();
 	msleep(1000);
 	/*
 	servo(3, 1000);
@@ -28,3 +71,15 @@ int main()
 	disable_servos();
 	return 0;
 }
+#endif
+
+//#define CAMERA_TEST
+#ifdef CAMERA_TEST
+int main() {
+	enable_servos();
+	
+	
+	disable_servos();
+	return 0;
+}
+#endif
