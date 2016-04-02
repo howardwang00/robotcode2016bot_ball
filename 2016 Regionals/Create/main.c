8f81542d95e1efd3c5b9bc50548702cc7b3de2dc
whackazombie <kevingao26@gmail.com>
2016-04-01 22:00:27 -0700

    Updating drive path

--- 2016 Regionals/Create/main.c
+++ 2016 Regionals/Create/main.c
@@ -1,20 +1,33 @@
-// Created on Sun January 31 2016
+#include "kipr/botball.h"
+#include "createDrive.h"
+#include "generic.h"
 
-#include "createDrive.h";
-#include "generic.h";
-#include "newmenu.h";
-#include "singing.h";
+#define ARM_UP 500
+#define CLAW_OPEN 200
+#define CLAW_CLOSE 1O00
+#define ARM_DOWN -1
 
 int main()
 {
-	
+     printf("a\n");
+     printf("b\n");
+	//create_full();
+    printf("c\n");
 	create_connect();
-	create_forward(50, 100);
-	create_right(90, 30, 100);
-	create_forward(50, 100);
-	create_block();
-	
-	
+    printf("d\n");
+    enable_servos();
+    servo_set(2, ARM_UP, 0.5);
+	create_forward(810, 150);
+	create_right(75, 30, 120);
+  servo_set(0, CLAW_OPEN, 1);
+	create_forward(500, 150);
+  	create_block();
+ 
+     //servo_set(0, X , 0.2);
+     servo_set(2, CLAW_DOWN, 0.5);
+     create_backward(270, 100);
+     disable_servos();
+  	
 	create_disconnect();
-	return 0;
+     return 0;
 }
