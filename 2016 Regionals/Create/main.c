6ee02073051a1ede143c827ace32bd3015d34a8c
whackazombie <kevingao26@gmail.com>
2016-04-03 16:38:58 -0700

    Get rekt Ethan

--- 2016 Regionals/Create/main.c
+++ 2016 Regionals/Create/main.c
@@ -1,4 +1,3 @@
-#include "kipr/botball.h"
 #include "createDrive.h"
 #include "generic.h"
 
@@ -6,31 +5,35 @@
 #define CLAW_OPEN 700
 #define CLAW_CLOSE 1300
 #define ARM_DOWN 1980
-#define FORWARD_A 500
+#define FORWARD_A 615
 #define FORWARD_B 150
 
 int main()
 {
-	
-	//create_full();
+    //create_full();
+    int a = 1;
     printf("c\n");
-	//create_connect();
-
-	
+    create_connect();
     printf("d\n");
     set_servo_position(0, ARM_UP);
     set_servo_position(2, CLAW_CLOSE);
     printf("e\n");
     enable_servos();
-	/*create_forward(FORWARD_A, 150);
-	create_right(76, 30, 120);
+  
+    create_forward(FORWARD_A, 150);
+	create_right(70, 30, 120);
     create_forward(FORWARD_B, 150);
-    create_left(40, 30, 120);
-	create_forward(300, 150);*/
-    msleep(1000);
-    set_servo_position(0, ARM_DOWN);
-    set_servo_position(2, CLAW_OPEN);
+    create_left(7.5, 30, 120);
+	create_forward(100, 150);
+  
+    msleep(12000);
+    a = 0;
+    if (a == 0) {
+    	set_servo_position(0, ARM_DOWN);
+    	set_servo_position(2, CLAW_OPEN);
     //create_block();
+    }
+  
      printf("f\n");
      msleep(2000);
      set_servo_position(2, 1370);
@@ -44,11 +47,11 @@ int main()
      msleep(500);
      set_servo_position(0, 1500);
      msleep(500);
+  
      //create_backward(270, 100);
      msleep(1000);
      disable_servos();
-  	
-	//create_disconnect();
-    ao();
+  	 create_disconnect();
+     ao();
      return 0;
 }
