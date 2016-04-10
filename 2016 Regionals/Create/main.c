8a9b377840f1a5de576ce3b488505561288c822a
henrywang008 <henrywang008@gmail.com>
2016-04-09 21:49:32 -0700

    updated the create
    Got the composter and trying to get it in the box. Currently making
    small changes to paths and turns for the box and the botguy. Botguy
    almost working.

--- 2016 Regionals/Create/main.c
+++ 2016 Regionals/Create/main.c
@@ -1,39 +1,39 @@
 #include "createDrive.h"
 #include "generic.h"
 
-#define ARM_UP 350
-#define CLAW_OPEN 700
-#define CLAW_CLOSE 1350
-#define ARM_DOWN 1980
-#define FORWARD_A 630
-#define FORWARD_B 150
+#define ARM_UP 400
+#define CLAW_OPEN 630
+#define CLAW_CLOSE 1300
+#define ARM_DOWN 2000
+#define ARM_PORT 0
+#define CLAW_PORT 2
 
 int main()
 {
+    
     //set up
-    //create_full();
-    int a = 1;
+    create_full();
     create_connect();
-    set_servo_position(0, ARM_UP);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(ARM_PORT, ARM_UP);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     enable_servos();
     
     //move to botguy
-    create_forward(FORWARD_A, 200);
-	create_right(64, 30, 120);
-    create_forward(FORWARD_B, 200);
-	create_forward(415, 200);
+    create_forward(630, 200);
+	create_right(66, 30, 120);
+    create_forward(150, 200);
+	create_forward(390, 200);
     create_stop();
     create_block();
 
     //open claw and set arm to get botguy
     msleep(1000);
-    set_servo_position(0, ARM_DOWN);
-    set_servo_position(2, CLAW_OPEN);
+    set_servo_position(ARM_PORT, ARM_DOWN);
+    set_servo_position(CLAW_PORT, CLAW_OPEN);
     msleep(2000);
-    set_servo_position(2, 1370);
-    msleep(500);
-    set_servo_position(2, 1400);
+    set_servo_position(CLAW_PORT, 1370);
+    msleep(300);
+    set_servo_position(CLAW_PORT, 1400);
     msleep(1000);
   
     //move it close to the pvc pipe so it doesn't need to move much over the ground
@@ -43,91 +43,163 @@ int main()
     msleep(300);
   
     //move arm up slowly so botguy doesn't fall off
-    set_servo_position(0, 1800);
+    set_servo_position(ARM_PORT, 1800);
     msleep(500);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    set_servo_position(0, 1650);
+    set_servo_position(ARM_PORT, 1650);
     msleep(500);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    set_servo_position(0, 1500);
+    set_servo_position(ARM_PORT, 1500);
     msleep(500);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    set_servo_position(0, 1420);
+    set_servo_position(ARM_PORT, 1420);
     msleep(500);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
   
     //get botguy to our side
-    create_backward(175, 50);
+    create_backward(155, 50);
     create_block();
     create_stop();
-    msleep(300);
+    msleep(500);
   
     //get botguy on ramp
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    set_servo_position(0, ARM_DOWN);
+    set_servo_position(ARM_PORT, 1450);
     msleep(500);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    create_left(21.69, 30, 37.5);
+    create_left(15, 30, 37.5);
     create_block();
     create_stop();
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(ARM_PORT, 1800);
     msleep(500);
-    set_servo_position(0, 1800);
-    create_forward(100, 35);
+    create_forward(145, 35);
     create_block();
     create_stop();
-    set_servo_position(2, CLAW_OPEN);
-    msleep(300);
-    set_servo_position(0, 900); 
-    msleep(300);
+    create_left(4, 30, 37.5);
+    set_servo_position(CLAW_PORT, 850);
+    msleep(1000);
+    set_servo_position(ARM_PORT, 900); 
+    msleep(500);
   
     //reset
-    create_backward(72, 150);
+    create_backward(180, 300);
     
     //box stuff
-    create_right(90, 30, 150);
-    create_forward(320, 150);
-    create_left(8, 30, 37.5);
+    create_right(76.9, 30, 150);
+    create_forward(310, 150);
     create_stop();
     create_block();
-
-  
-     //open claw and set arm to get box
-    msleep(1000);
-    set_servo_position(0, ARM_DOWN);
-    set_servo_position(2, CLAW_OPEN);
-    msleep(2000);
-    set_servo_position(2, 1370);
     msleep(500);
-    set_servo_position(2, 1400);
-    msleep(1000);
   
+    //open claw and set arm to get box
+    set_servo_position(CLAW_PORT, CLAW_OPEN);
+    msleep(500);
+    set_servo_position(ARM_PORT, ARM_DOWN);
+    msleep(500);
+    set_servo_position(CLAW_PORT, 1370);
+    msleep(500);
+    set_servo_position(CLAW_PORT, 1400);
+    msleep(500);
+  
+    //take it closer to the pole and move back forward
     create_backward(140, 75);
     create_block();
     create_stop();
-  
-    //move arm up
-    set_servo_position(2, CLAW_CLOSE);
+    set_servo_position(CLAW_PORT, 1000);
     msleep(500);
-    set_servo_position(0, 600);
+    set_servo_position(ARM_PORT, 2047);
     msleep(500);
-    create_backward(169, 50);
+    create_forward(70, 75);
     create_block();
     create_stop();
-    create_right(130, 30, 30);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    msleep(500);
+    
+  
+    //move arm up
+    msleep(500);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    msleep(500);
+    set_servo_position(0, 350);
+    msleep(500);
+    
+    //get the box to the colored line
+    create_backward(70, 75);
+    create_block();
+    create_stop();
+    create_right(143, 30, 100);
     create_block();
     create_stop();
     create_backward(60, 50);
     create_block();
     create_stop();
-    set_servo_position(0, ARM_DOWN); 
+    set_servo_position(ARM_PORT, ARM_UP);
+    msleep(500);
+    set_servo_position(ARM_PORT, 1000);
+    msleep(500);
+    set_servo_position(ARM_PORT, ARM_DOWN); 
+    msleep(500);
+    set_servo_position(CLAW_PORT, CLAW_OPEN);
+    msleep(500);
+    set_servo_position(ARM_PORT, ARM_UP);
+    msleep(500);
+  
+    //turn and get composter
+    create_block();
+    create_stop();
+    create_left(82, 30, 100);
+    create_block();
+    create_stop();
+    create_forward(110, 100);
+    create_block();
+    create_stop();
+    set_servo_position(ARM_PORT, 1800);
+    msleep(500);
+    set_servo_position(CLAW_PORT, CLAW_OPEN);
+    msleep(500);
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    msleep(500);
+    set_servo_position(ARM_PORT, 500);
+    msleep(500);
+  
+    //get the composter to the box
+    create_block();
+    create_stop();
+    create_right(46, 30, 100);
+    create_block();
+    create_stop();
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    msleep(500);
+    create_right(47, 30, 100);
+    create_block();
+    create_stop();
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    msleep(500);
+    create_right(46, 30, 100);
+    create_block();
+    create_stop();
+    set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    msleep(500);
+    create_forward(1200, 150);
+    create_block();
+    create_stop();
+  
+    //dropping composter
+    set_servo_position(ARM_PORT, ARM_DOWN);
+    msleep(500);
+    set_servo_position(CLAW_PORT, CLAW_OPEN);
+    msleep(500);
+    set_servo_position(ARM_PORT, ARM_UP);
+    msleep(500);
+  
     //end program
     disable_servos();
     create_disconnect();
