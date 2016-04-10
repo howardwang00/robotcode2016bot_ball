9f160913415d41ae53c6c578ab7530f3e37f62a0
henrywang008 <henrywang008@gmail.com>
2016-04-10 16:58:47 -0700

    Decided we are getting the balls from the top

--- 2016 Regionals/Create/main.c
+++ 2016 Regionals/Create/main.c
@@ -7,6 +7,9 @@
 #define ARM_DOWN 2000
 #define ARM_PORT 0
 #define CLAW_PORT 2
+#define POKER_PORT 3
+#define POKER_UP 2047
+#define POKER_DOWN 0
 
 int main()
 {
@@ -16,13 +19,14 @@ int main()
     create_connect();
     set_servo_position(ARM_PORT, ARM_UP);
     set_servo_position(CLAW_PORT, CLAW_CLOSE);
+    set_servo_position(POKER_PORT, POKER_DOWN);
     enable_servos();
     
     //move to botguy
     create_forward(630, 200);
-	create_right(66, 30, 120);
+	create_right(65, 30, 120);
     create_forward(150, 200);
-	create_forward(390, 200);
+	create_forward(410, 200);
     create_stop();
     create_block();
 
@@ -73,7 +77,7 @@ int main()
     msleep(500);
     set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    create_left(15, 30, 37.5);
+    create_left(17, 30, 37.5);
     create_block();
     create_stop();
     set_servo_position(CLAW_PORT, CLAW_CLOSE);
@@ -83,7 +87,7 @@ int main()
     create_forward(145, 35);
     create_block();
     create_stop();
-    create_left(4, 30, 37.5);
+    //create_left(4, 30, 37.5);
     set_servo_position(CLAW_PORT, 850);
     msleep(1000);
     set_servo_position(ARM_PORT, 900); 
@@ -93,7 +97,7 @@ int main()
     create_backward(180, 300);
     
     //box stuff
-    create_right(76.9, 30, 150);
+    create_right(65, 30, 150);
     create_forward(310, 150);
     create_stop();
     create_block();
@@ -135,7 +139,7 @@ int main()
     create_backward(70, 75);
     create_block();
     create_stop();
-    create_right(143, 30, 100);
+    create_right(139, 30, 100);
     create_block();
     create_stop();
     create_backward(60, 50);
@@ -155,13 +159,13 @@ int main()
     //turn and get composter
     create_block();
     create_stop();
-    create_left(82, 30, 100);
+    create_left(85, 30, 100);
     create_block();
     create_stop();
     create_forward(110, 100);
     create_block();
     create_stop();
-    set_servo_position(ARM_PORT, 1800);
+    set_servo_position(ARM_PORT, 1844);
     msleep(500);
     set_servo_position(CLAW_PORT, CLAW_OPEN);
     msleep(500);
@@ -171,34 +175,36 @@ int main()
     msleep(500);
   
     //get the composter to the box
-    create_block();
-    create_stop();
-    create_right(46, 30, 100);
-    create_block();
-    create_stop();
-    set_servo_position(CLAW_PORT, CLAW_CLOSE);
-    msleep(500);
-    create_right(47, 30, 100);
+    create_right(128, 30, 100);
     create_block();
     create_stop();
     set_servo_position(CLAW_PORT, CLAW_CLOSE);
     msleep(500);
-    create_right(46, 30, 100);
+    //create_forward(1000, 150);
+  
+  while(get_create_lbump() == 0) {
+    create_forward(10, 150);
     create_block();
-    create_stop();
-    set_servo_position(CLAW_PORT, CLAW_CLOSE);
-    msleep(500);
-    create_forward(1200, 150);
+  }
+  
+    create_right(20, 30, 150);
+    create_forward(150, 150);
     create_block();
     create_stop();
   
     //dropping composter
-    set_servo_position(ARM_PORT, ARM_DOWN);
     msleep(500);
     set_servo_position(CLAW_PORT, CLAW_OPEN);
     msleep(500);
+    set_servo_position(CLAW_PORT, 1000);
+    msleep(500);
     set_servo_position(ARM_PORT, ARM_UP);
     msleep(500);
+  
+    //move to the right position to get the ballz down
+    set_servo_position(POKER_PORT, POKER_UP);
+    msleep(500);
+    create_right(90, 30, 100);
   
     //end program
     disable_servos();
